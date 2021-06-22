4bede0990b9f42218af4eca27695eabccb3b2c32
accidentlywoo <accidentlywoo@gmail.com>
2021-06-23 05:52:33 +0900

    test : before each 픽스처 셋업

--- src/test/java/nextstep/subway/line/SectionAcceptanceTest.java
+++ src/test/java/nextstep/subway/line/SectionAcceptanceTest.java
@@ -1,19 +1,41 @@
 package nextstep.subway.line;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import nextstep.subway.AcceptanceTest;
+import nextstep.subway.line.dto.LineRequest;
+import nextstep.subway.line.dto.LineResponse;
+import nextstep.subway.station.dto.StationRequest;
+import nextstep.subway.station.dto.StationResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static nextstep.subway.LineStationFixture.*;
+
 public class SectionAcceptanceTest extends AcceptanceTest {
+	LineResponse lineResponse;
 
 	@BeforeEach
-	public void setUp() {
+	public void setUp() throws JsonProcessingException {
 		// given, line 생성 <- 상행약, 하행역 station 등록, 종점 및 거리 세팅
+		StationRequest stationRequest1 = new StationRequest();
+		stationRequest1.setName("잠실역");
+		StationResponse stationResponse1 = 지하철역세팅_return_dto(stationRequest1);
+
+		StationRequest stationRequest2 = new StationRequest();
+		stationRequest2.setName("강남역");
+		StationResponse stationResponse2 = 지하철역세팅_return_dto(stationRequest2);
+
+		LineRequest lineRequest = new LineRequest("2호선", "yellow", stationResponse1.getId(), stationResponse2.getId(), 7);
+		lineResponse = 노선정보세팅_return_dto(lineRequest);
 	}
 
 	@Test
 	public void 종점사이의_새로운역_상행연결_등록_성공() {
 		// given 상행역정보
+		Long upStationId = lineResponse.getUpStationId();
+		
 		// when + 새로운 station 생성 및 등록, 양 좀점보다 짧은 구간정보 역은 1m(?)차지
 		// then 상행 - 새로운 구간 -
 	}
