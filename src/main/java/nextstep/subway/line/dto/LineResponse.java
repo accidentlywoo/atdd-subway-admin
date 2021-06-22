62cd4f809c6484b5a06f3707b4a4eb81e10bff97
accidentlywoo <accidentlywoo@gmail.com>
2021-06-23 05:55:21 +0900

    feat : line 생성 response 객체 구현

--- src/main/java/nextstep/subway/line/dto/LineResponse.java
+++ src/main/java/nextstep/subway/line/dto/LineResponse.java
@@ -1,67 +1,95 @@
 package nextstep.subway.line.dto;
 
 import nextstep.subway.line.domain.Line;
+import nextstep.subway.line.domain.Section;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class LineResponse {
-    private Long id;
-    private String name;
-    private String color;
-    private LocalDateTime createdDate;
-    private LocalDateTime modifiedDate;
+	private Long id;
+	private String name;
+	private String color;
+	private Long upStationId;
+	private Long downStationId;
+	private LocalDateTime createdDate;
+	private LocalDateTime modifiedDate;
 
-    public LineResponse() { }
+	public LineResponse() {
+	}
 
-    public LineResponse(Long id, String name, String color, LocalDateTime createdDate, LocalDateTime modifiedDate) {
-        this.id = id;
-        this.name = name;
-        this.color = color;
-        this.createdDate = createdDate;
-        this.modifiedDate = modifiedDate;
-    }
+	public LineResponse(Long id, String name, String color, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+		this.id = id;
+		this.name = name;
+		this.color = color;
+		this.createdDate = createdDate;
+		this.modifiedDate = modifiedDate;
+	}
 
-    public static LineResponse of(Line line) {
-        return new LineResponse(line.id(), line.name(), line.color(), line.getCreatedDate(), line.getModifiedDate());
-    }
+	public static LineResponse of(Line line) {
+		LineResponse lineResponse = new LineResponse(
+				line.id(), line.name(), line.color(), line.getCreatedDate(), line.getModifiedDate()
+		);
 
-    public Long getId () {
-        return id;
-    }
+		lineResponse.setUpStationId(line.getUpStation().getId());
+		lineResponse.setDownStationId(line.getDownStation().getId());
 
-    public void setId (Long id) {
-        this.id = id;
-    }
+		return lineResponse;
+	}
 
-    public String getName () {
-        return name;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setName (String name) {
-        this.name = name;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public String getColor () {
-        return color;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setColor (String color) {
-        this.color = color;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public LocalDateTime getCreatedDate () {
-        return createdDate;
-    }
+	public String getColor() {
+		return color;
+	}
 
-    public void setCreatedDate (LocalDateTime createdDate) {
-        this.createdDate = createdDate;
-    }
+	public Long getUpStationId() {
+		return upStationId;
+	}
 
-    public LocalDateTime getModifiedDate () {
-        return modifiedDate;
-    }
+	public void setUpStationId(Long upStationId) {
+		this.upStationId = upStationId;
+	}
 
-    public void setModifiedDate (LocalDateTime modifiedDate) {
-        this.modifiedDate = modifiedDate;
-    }
+	public Long getDownStationId() {
+		return downStationId;
+	}
+
+	public void setDownStationId(Long downStationId) {
+		this.downStationId = downStationId;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDateTime getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(LocalDateTime modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
 }
